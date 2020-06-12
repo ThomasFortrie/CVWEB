820b56f019befbe08ce5b3002ce689b60a571be8
ThomasFortrie <thomas.fortrie@hotmail.fr>
2020-06-12 17:45:48 +0200

    debut Dev JS pour la div principale

--- JS/littlejs.js
+++ JS/littlejs.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     }
 
 
-    $('#photoProfil').hover(function(e){
+    $('#photoProfil').hover(function (e) {
 
 
 
@@ -18,17 +18,22 @@ $(document).ready(function () {
         e.stopPropagation();
         e.preventDefault();
 
-        if (event.target.classList.contains("thomas")){
+        if (event.target.classList.contains("thomas")) {
             console.log("test tom");
+            eraseChangeDiv();
+
         }
-        if (event.target.classList.contains("thomas")){
-            
+
+        if (event.target.classList.contains("thomas")) {
+            eraseChangeDiv();
         }
-        if (event.target.classList.contains("thomas")){
-            
+
+        if (event.target.classList.contains("thomas")) {
+            eraseChangeDiv();
         }
-        if (event.target.classList.contains("thomas")){
-            
+
+        if (event.target.classList.contains("thomas")) {
+            eraseChangeDiv();
         }
 
 
@@ -41,6 +46,21 @@ $(document).ready(function () {
 
 
 
+    // Efface et vide la changeDiv
+    function eraseChangeDiv() {
+        $('#changeDiv').fadeOut(500);
+        setTimeout(function () {
+            $('#changeDiv').empty();
+        }, 500)
+    }
+
+
+    // Re-création et remplacement texte dans la div principale
+    // la variable text sert à remplir la div, et la variable classe sera l'attribut class de la div
+    function changeDivGo($text, $class) {
+        
+
+    }
 
 
 
@@ -50,26 +70,22 @@ $(document).ready(function () {
 
 
 
+    // applique les nouvelles tailles à la div principales
+    function setSizes() {
+        var height = $(window).height();
+        var width = $(window).width();
+        console.log(height + " " + width)
+        $('#realBody').css({
+            "min-height": height,
+            "min-width": width
+        })
+        $('#body').css({
+            "height": height,
+            "width": width
+        })
+    }
 
 
-
-
-// applique les nouvelles tailles à la div principales
-function setSizes() {
-    var height = $(window).height();
-    var width = $(window).width();
-    console.log(height + " " + width)
-    $('#realBody').css({
-        "min-height": height,
-        "min-width": width
-    })
-    $('#body').css({
-        "height": height,
-        "width": width
-    })
-}
-
-    
 
 
 })
