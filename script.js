e1f82596bdd5e9c840373705ad5f12cebffdbc8c
FortrieThomas <thomas.fortrie@live.fr>
2020-08-25 16:21:09 +0200

    add scroll aparition

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const ratio = 0.4;
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+
+  const handleIntersect = function(entries, observer){
+      entries.forEach(function(entry){
+          console.log(entry.intersectionRatio)
+          if(entry.intersectionRatio > ratio){
+              entry.target.classList.add('reveal-visible')
+              observer.unobserve(entry.target)
+          }else{
+          }
+
+      })
+      console.log("handleIntersect")
+  }
+  
+  const observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(document.querySelector(".reveal"))
